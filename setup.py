f823d4b76bfc50e0bae3ab03fb60c2e3b1474b23
ajacassi <a.jacassi@vargroup.it>
2025-08-27 09:29:30 +0200

    Update dependencies in requirements and setup, bump version to 2.1.3

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ with open("DependiPy/version.py", "r") as f:
 version = version[0].split("'")[1]
 
 argparse = 'argparse==1.4.0'
-tqdm = 'tqdm==4.64.1'
+tqdm = 'tqdm>=4.64.1'
 pandas = 'pandas==2.0.0'
-numpy = 'numpy==1.23.5'
+numpy = 'numpy>=1.23.5'
 
 setup(
     name='DependiPy',
